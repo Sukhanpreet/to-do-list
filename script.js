9d49b5d02732181545701e7e30324f9741edd783
Sukhanpreet <sukhanpreet356@gmail.com>
2022-07-13 22:53:33 +0530

    2nd commit
    add delete task function

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 let taskArray=[];
 
-function addTask(){
-    let task=document.getElementById('task').value;
-    console.log(task);
-    taskArray.push(task);
-    console.log(taskArray);
 
-    const ul=document.getElementById('list');
-    
-    const li=document.createElement('li');
-    
-    ul.append(li);
-
-    li.innerText=task;
-
-    li.classList.add('list-items');
+function deletetask(currentitem,button){
+    button.remove();
+    currentitem.remove();  
 }
+
+function addTask(){
+
+    let task=document.getElementById('task').value;
+
+    if(task!= ''){
+        
+        taskArray.push(task);
+    
+        const ul=document.getElementById('list');
+        const li=document.createElement('li');
+        const button=document.createElement('input');
+        
+        button.setAttribute('type','button');
+        button.setAttribute('value','Delete task');
+        button.setAttribute('onclick','deletetask(this.previousElementSibling,this)');
+    
+        li.innerText=task;
+        ul.append(li);
+        ul.append(button);
+    
+        li.classList.add('list-items');
+    }
+    else{
+        alert("Please Enter Task");
+    }
+    
+}
+
+
+
 
