27b6cacc3ff29c2a31eba1bc5c971a70f4143322
Sukhanpreet <sukhanpreet356@gmail.com>
2022-07-14 15:19:04 +0530

    3rd commit
    styles added

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let taskArray=[];
 
+const ul=document.getElementById('list');
 
-function deletetask(currentitem,button){
-    button.remove();
-    currentitem.remove();  
+function deletetask(currentitem){
+    currentitem.remove(); 
 }
 
 function addTask(){
@@ -11,27 +10,28 @@ function addTask(){
     let task=document.getElementById('task').value;
 
     if(task!= ''){
-        
-        taskArray.push(task);
-    
-        const ul=document.getElementById('list');
+
         const li=document.createElement('li');
         const button=document.createElement('input');
         
         button.setAttribute('type','button');
-        button.setAttribute('value','Delete task');
-        button.setAttribute('onclick','deletetask(this.previousElementSibling,this)');
+        button.setAttribute('value','Done');
+        
+        button.setAttribute('onclick','deletetask(this.parentElement)');
     
         li.innerText=task;
         ul.append(li);
-        ul.append(button);
-    
+        li.append(button);
+   
         li.classList.add('list-items');
+    
+        document.getElementById('task').value=``;
+
     }
     else{
         alert("Please Enter Task");
     }
-    
+
 }
 
 
